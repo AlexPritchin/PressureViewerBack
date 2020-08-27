f73b6d91827ef55dc89db43dccae36c53142dae0
Oleksii Prytchyn <alpritchin@ukr.net>
2020-08-27 19:20:31 +0300

    Auth routes Signup validation added

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const { body } = require('express-validator/check');
+
+const User = require('../models/user_model');
 
 const router = express.Router();
 
-router.put('/signup');
+router.put('/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Email is invalid')
+    .custom((value, { req }) => {
+      return User.findOne({ email: value }).then(user => {
+        if (user) {
+          return Promise.reject('User with such e-mail already exists');
+        }
+      });
+    })
+    .normalizeEmail(),
+  body('password').trim().not().isEmpty().isLength({ min: 6 }).isAlphanumeric(),
+]);
 
 module.exports = router;
