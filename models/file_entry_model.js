d58078cb69516231d104b59e2ab74f3bf77e217c
Oleksii Prytchyn <alpritchin@ukr.net>
2020-09-17 17:24:38 +0300

    File entry model: user id added

--- models/file_entry_model.js
+++ models/file_entry_model.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 
 const fileEntrySchema = new Schema({
     dateModified: Number,
-    fileName: String
+    fileName: String,
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 });
 
 module.exports = mongoose.model('FileEntry', fileEntrySchema);
